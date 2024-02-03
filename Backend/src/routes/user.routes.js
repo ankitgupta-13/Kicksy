4c9541aeb6c28634aa34f9bf1feb7709fc58be16
ankitgupta-13 <guptankit0522@gmail.com>
2024-02-03 16:27:36 +0530

    Update user.controller.js and user.routes.js

--- Backend/src/routes/user.routes.js
+++ Backend/src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { loginUser, registerUser } from "../controllers/user.controller.js";
+import { loginUser, registerUser } from "../controllers/user.controllers.js";
 
 const router = Router();
 
