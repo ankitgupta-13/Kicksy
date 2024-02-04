eb7733266a9c52f0d9ac1d469b8cb69e1625588a
ankitgupta-13 <guptankit0522@gmail.com>
2024-02-04 11:48:07 +0530

    Configured redux and react-router-dom

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,7 +1,23 @@
 import "./App.css";
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Cart from "./pages/Cart/Cart";
 
 function App() {
-  return <>Hello World!</>;
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <>
+        <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
+      </>
+    )
+  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
