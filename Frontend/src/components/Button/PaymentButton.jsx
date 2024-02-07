d8d11de2a79e32a7ab4181ab9848570b527981df
Tanish Rastogi <67361189+tanishrastogi@users.noreply.github.com>
2024-02-07 08:01:17 +0000

    07-02-2024 commit-T2 payment-integration

--- Frontend/src/components/Button/PaymentButton.jsx
+++ Frontend/src/components/Button/PaymentButton.jsx
@@ -3,15 +3,14 @@ import React from "react";
 const PaymentButton = (props)=>{
     const {amount} = props;
     const checkOutHandler = async()=>{
-        const rpay_key = await fetch("http://localhost:3000/api/user/payments/get-key" , {
+        const rpay_key = await fetch("https://fantastic-umbrella-xxv4xq9x994c977v-8005.app.github.dev/api/user/payments/get-key" , {
             method:"get",
             headers:{
                 "content-type":'application/json'
             }
         })
         const key = await rpay_key.json();
-        console.log( key , amount);
-        const data = await fetch("http://localhost:3000/api/user/payments/make-payment" , {
+        const data = await fetch("https://fantastic-umbrella-xxv4xq9x994c977v-8005.app.github.dev/api/user/payments/make-payment" , {
           method:"post",
           headers:{
             "content-type":"application/json"
@@ -21,6 +20,34 @@ const PaymentButton = (props)=>{
 
         const payment = await data.json();
         console.log(payment);
+        const options = {
+          key,
+          amount,
+          currency:"INR",
+          name:"Charlie",
+          description:"Razorpay tutorial",
+          image:"",
+          order_id:payment.data,
+          callback_url:"https://fantastic-umbrella-xxv4xq9x994c977v-8005.app.github.dev/api/user/payments/verify-payment",
+          prefill:{
+            name:"Tanish Rastogi",
+            email:"charliefernandis28@gmail.com",
+            contact:"1234567890",
+          },
+          notes:{
+            "address":"abcdefghijkl"
+          },
+          theme:{
+            "color":"#3399cc"
+          }
+
+        }
+
+        const razor = new window.Razorpay(options);
+
+        razor.open();
+
+
         
       }
     return ( <div>
@@ -32,12 +59,3 @@ const PaymentButton = (props)=>{
 export default PaymentButton;
 
 
-// import React from 'react'
-
-// const PaymentButton = () => {
-//   return (
-//     <div>PaymentButton</div>
-//   )
-// }
-
-// export default PaymentButton
