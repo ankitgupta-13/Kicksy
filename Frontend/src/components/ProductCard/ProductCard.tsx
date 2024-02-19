be467b4c9136fd8ecdaeb02334c02649daf15916
Tanish Mishra <tanishmishra97@gmail.com>
2024-02-19 20:20:56 +0530

    fix(ProductCard): media queries added for 430px

--- Frontend/src/components/ProductCard/ProductCard.tsx
+++ Frontend/src/components/ProductCard/ProductCard.tsx
@@ -2,6 +2,8 @@ import style from './ProductCard.module.css'
 import Shoes from '../../assets/adidas.png' // Dummy Image of Shoes
 import shoesColor from '../../assets/shoes_color.png' // Dummy Image of Shoes Color
 import ColorCard from '../colorCard/colorCard' 
+import Fire from '../../assets/images/fire.png'
+import Add from '../../assets/images/add.png'
 import { useState } from 'react'
 const BestSeller = (product: any) => {
     const [shoesColorData,setShoesColorData] = useState([
@@ -25,6 +27,10 @@ const BestSeller = (product: any) => {
 
   return (
     <div className={style.container}>
+     <div className={style.container__header}>
+        <div className={style.container__hotitem}><img src={Fire} alt='error'/></div>
+        <img src={Add} className={style.container__addcard} alt='error'/>
+     </div>
         <div className={style.shoes}>
         <img src={imagesrc} className={style.shoes__image}/>
         </div>
@@ -41,6 +47,8 @@ const BestSeller = (product: any) => {
                 <div className={style.shoes__price}>Rs. 20,456</div>
             </div>
            <p className={style.shoes__tags}>Road Running / Weight + Strength Training / Primal Movement / Calisthenics / Active Everyday</p>
+           <div className={style.mobile__shoes_price}>INR 56,300</div>
+           <div className={style.mobile__expected_price}>64,300</div>
            <p className={style.shoes__tagline}>A barefoot sneaker for strength training, animal flow and light
 running</p>
         </div>
