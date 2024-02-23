239f0e84951c615c648c1256ff966b18ae064208
Crucie <amaymishra100@gmail.com>
2024-02-24 00:56:27 +0530

    Add Custom Anime Card

--- Frontend/src/pages/Anime/Anime.tsx
+++ Frontend/src/pages/Anime/Anime.tsx
@@ -1,8 +1,31 @@
 import React from 'react'
+import style from "./Anime.module.css";
+import img from "../../assets/images/AnimePage/bakugoS.png"
+import img2 from "../../assets/images/AnimePage/SpideyS.png"
+  
 
 const Anime = () => {
   return (
-    <div>Anime</div>
+    <div>
+      <div className={style.Container}>
+          <div className={style.Card1}>
+            <div className={style.head}> Explore our customized anime sneakers</div>
+            <div className={style.Circle1}>
+              
+              {/* <div className={style.image}> LOL</div> */}
+              <img className={style.image} src={img}></img>
+            </div>
+          </div>
+        </div>
+
+      <div className={style.Container}>
+        <div className={style.Card2}>
+          <div className={style.Circle2}>
+          <img className={style.image} src={img2}></img>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
