ad264db6c3e11e4fb73a38791fcbaead956e2de7
RaunikS <raunik333@gmail.com>
2024-02-24 01:07:23 +0530

    Home Page

--- Frontend/src/pages/Home/Home.tsx
+++ Frontend/src/pages/Home/Home.tsx
@@ -4,6 +4,18 @@ import style from "./Home.module.css";
 import ProductCard from "../../components/ProductCard/ProductCard";
 import SliderCard from "../../components/SliderCard/SliderCard";
 import { Button, Container } from "../../components";
+import HeroSection from "../../components/HeroSection/HeroSection";
+
+//Importing Images
+import GirlPic1 from "../../assets/images/GirlPic1.png"
+import MenPic1 from "../../assets/images/MenPic1.png"
+import KidPic1 from "../../assets/images/KidPic1.png"
+import JordanLogo from "../../assets/images/JordanLogo.png"
+import NikeLogo from "../../assets/images/NikeLogo.png"
+import AdidasLogo from "../../assets/images/AdidasLogo.png"
+import AnimeShoe1 from "../../assets/images/AnimeShoe1.png"
+import CustomNike from "../../assets/images/CustomNike.png"
+
 
 import { useNavigate } from "react-router-dom";
 
@@ -29,15 +41,15 @@ const Home = () => {
       <SliderCard />
       <div className={style.Gender}>
         <div className={style.GenderContainer}>
-          <div className={style.GenderBox}></div>
+          <div className={style.GenderBox}><img src={GirlPic1} alt="" /></div>
           <div className={style.GenderBoxTitle}>Women</div>
         </div>
         <div className={style.GenderContainer}>
-          <div className={style.GenderBox}></div>
+          <div className={style.GenderBox}><img src={MenPic1} alt="" /></div>
           <div className={style.GenderBoxTitle}>Men</div>
         </div>
         <div className={style.GenderContainer}>
-          <div className={style.GenderBox}></div>
+          <div className={style.GenderBox}><img src={KidPic1} alt="" /></div>
           <div className={style.GenderBoxTitle}>Kids</div>
         </div>
         <div className={style.SaleBox}>
@@ -47,31 +59,27 @@ const Home = () => {
 
       <div className={style.CompanyContainer}>
         <div className={style.CompanyItemBox}>
-          <div className={style.CompanyItem}></div>
+          <div className={style.CompanyItem}><img src={JordanLogo} alt="" /></div>
           <div className={style.CompanyItemTitle}>Jordan</div>
 
         </div>
         <div className={style.CompanyItemBox}>
-          <div className={style.CompanyItem}></div>
+          <div className={style.CompanyItem}><img src={NikeLogo} alt="" /></div>
           <div className={style.CompanyItemTitle}>Jordan</div>
         </div>
         <div className={style.CompanyItemBox}>
-          <div className={style.CompanyItem}></div>
+          <div className={style.CompanyItem}><img src={AdidasLogo} alt="" /></div>
           <div className={style.CompanyItemTitle}>Jordan</div>
         </div>
         <div className={style.CompanyItemBox}>
-          <div className={style.CompanyItem}></div>
+          <div className={style.CompanyItem}><img src={NikeLogo} alt="" /></div>
           <div className={style.CompanyItemTitle}>Jordan</div>
         </div>
         <div className={style.CompanyItemBox}>
-          <div className={style.CompanyItem}></div>
-        </div>
-        <div className={style.CompanyItemBox}>
-          <div className={style.CompanyItem}></div>
+          <img src={CustomNike} alt="" />
         </div>
       </div>
 
-      <p>Welcome to the home page</p>
       <div>
         <h1>New Arrivals</h1>
 
@@ -84,6 +92,9 @@ const Home = () => {
             );
           })}
         </div>
+        <HeroSection/>
+
+        
 
       </div>
     </Container>
