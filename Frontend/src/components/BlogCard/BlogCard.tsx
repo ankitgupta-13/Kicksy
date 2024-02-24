db1ceede4cae8b1ac7cdd1b3c0dd7db8be805b4e
Tanish Mishra <tanishmishra97@gmail.com>
2024-02-24 23:43:06 +0530

    fix(blogPage): blog card added

--- Frontend/src/components/BlogCard/BlogCard.tsx
+++ Frontend/src/components/BlogCard/BlogCard.tsx
@@ -1,12 +1,17 @@
 import style from './BlogCard.module.css'
-
-
-const BlogCard = ({blog}) => {
+import DummyImage from '../../assets/dummy_blog_image.png'
+const BlogCard = ({blog: any}) => {
   return (
     <div className={style.card}>
-        <img src={blog.imageurl}  className={style.image} alt="Blog image"></img>
-        <a>Games . {blog.createdAt}</a>
-        <h3>{blog.blogTitle}</h3>
+        <img src={DummyImage}  className={style.image} alt="Blog image"/>
+        <div className={style.card__info}>
+             <a>Games</a>
+             <span className={style.card__info_entity}>&#9679;</span>
+             <span className={style.card__info_date}>Feb 18, 2020</span>
+             </div>
+        <p className={style.card__paragraph}>Lorem ipsum dolor sit amet consectetur.</p>
+     
+        {/* <h3>{blog.blogTitle}</h3> */}
     </div>
   )
 }
--- Frontend/src/pages/Blogs/Blogs.tsx
+++ Frontend/src/pages/Blogs/Blogs.tsx
@@ -5,7 +5,27 @@ import { getAllBlogs } from '../../api/user.api';
 
 
 const Blog = () => {
-  const [bloglist, setBloglist] = useState([]);
+  const [bloglist, setBloglist] = useState(
+      [
+        {
+        id: 1,
+      },
+        {
+        id: 2,
+      },
+        {
+        id: 3,
+      },
+        {
+        id: 4,
+      },
+        {
+        id: 5,
+      },
+        {
+        id: 6,
+      },
+  ]);
 
   const getBlogs = async () => {
     const response = await getAllBlogs();
@@ -18,20 +38,18 @@ const Blog = () => {
 
   
   return (
-    <div>
+    <div className={style.container}>
       <div className={style.topbar}>
-        Blog
+   <span className={style.topbar__heading}>Blog</span>
+   <span className={style.topbar__subheading}>Home - <span >All Posts</span></span>
       </div>
       <div className={style.bloglist}>
-        {bloglist.map((blog) =>{
-          return (
-          <BlogCard blog={blog}/>
-          );
-        })
-        }
+         {bloglist.map((blogs)=>(
+          <BlogCard blog={blogs}/>
+         ))}
       </div>
       <div className={style.recent_blogs}>
-        <label>Stay-up-to-date</label>
+        <div className={style.recent_blogs_title}>Stay-up-to-date</div>
         <h2>RECENT POSTS</h2>
       </div>
     </div>
