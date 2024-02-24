6de14fa4cadc3cb0168b8e4509e811d00406339e
Crucie <amaymishra100@gmail.com>
2024-02-25 00:44:23 +0530

    Bebas Font added

--- Frontend/src/pages/Anime/Anime.tsx
+++ Frontend/src/pages/Anime/Anime.tsx
@@ -2,11 +2,12 @@ import React from 'react'
 import style from "./Anime.module.css";
 import img from "../../assets/images/AnimePage/bakugoS.png"
 import img2 from "../../assets/images/AnimePage/SpideyS.png"
+import { Button } from '@mui/material';
   
 
 const Anime = () => {
   return (
-    <div>
+    <div style={{display:'flex', flexDirection:'row'}}>
       <div className={style.Container}>
           
           <div className={style.Card1}>
@@ -25,6 +26,16 @@ const Anime = () => {
 
       <div className={style.Container}>
         <div className={style.Card2}>
+          <div className={style.head}> Sneaker Culture + Anime Artistry</div>
+            <div className={style.Content}> 
+                  We understand your love for both quality footwear and captivating anime designs. 
+                  That's why we've meticulously curated a collection that not only meets but exceeds your expectations.
+                  Each sneaker is a canvas, waiting to be adorned with the vibrant hues and
+                  intricate details of your favorite anime characters, ensuring that every step you take 
+                  is a testament to your uniq ue style and personality.    
+            </div>
+            <div className={style.head}> Sneaker Culture + Anime Artistry</div>
+            <div className={style.head}> Sneaker Culture + Anime Artistry</div>
           <div className={style.Circle2}>
           <img className={style.image} src={img2}></img>
           </div>
