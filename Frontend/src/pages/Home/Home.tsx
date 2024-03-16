af8cc405fbbab3e286ceaef9c6d0ee47adb0c865
ankitgupta-13 <guptankit0522@gmail.com>
2024-03-16 20:29:42 +0530

    Refactor code for better readability and maintainability

--- Frontend/src/pages/Home/Home.tsx
+++ Frontend/src/pages/Home/Home.tsx
@@ -36,85 +36,114 @@ const Home = () => {
       <div className={style.Gender}>
         <div className={style.GenderContainer}>
           <div className={style.GenderBox}>
-            <img src={GirlPic1} alt="" onClick={() => navigate(`/shop?gender=F`)}/>
+            <img
+              src={GirlPic1}
+              alt=""
+              onClick={() => navigate(`/shop?gender=F`)}
+            />
           </div>
           <div className={style.GenderBoxTitle}>Women</div>
         </div>
         <div className={style.GenderContainer}>
           <div className={style.GenderBox}>
-            <img src={MenPic1} alt="" onClick={() => navigate(`/shop?gender=M`)}/>
+            <img
+              src={MenPic1}
+              alt=""
+              onClick={() => navigate(`/shop?gender=M`)}
+            />
           </div>
-          <div className={style.GenderBoxTitle} >Men</div>
+          <div className={style.GenderBoxTitle}>Men</div>
         </div>
         <div className={style.GenderContainer}>
           <div className={style.GenderBox}>
-            <img src={KidPic1} alt="" onClick={() => navigate(`/shop?gender=K`)}/>
+            <img
+              src={KidPic1}
+              alt=""
+              onClick={() => navigate(`/shop?gender=K`)}
+            />
           </div>
           <div className={style.GenderBoxTitle}>Kids</div>
         </div>
         <div className={style.SaleBox}>
           <div className={style.Box}>
-            <span className={style.SaleBoxContent} onClick={() => navigate(`/shop?category=sale`)}>Sale</span>
+            <span
+              className={style.SaleBoxContent}
+              onClick={() => navigate(`/shop?category=sale`)}
+            >
+              Sale
+            </span>
           </div>
         </div>
       </div>
 
       <div className={style.CompanyContainer}>
-        <div className={style.CompanyItemBox} onClick={() => navigate(`/shop?brand=jordan`)}>
+        <div
+          className={style.CompanyItemBox}
+          onClick={() => navigate(`/shop?brand=jordan`)}
+        >
           <div className={style.CompanyItem}>
             <img src={JordanLogo} alt="" />
           </div>
           <div className={style.CompanyItemTitle}>Jordan</div>
         </div>
-        <div className={style.CompanyItemBox} onClick={() => navigate(`/shop?brand=jordan`)}>
+        <div
+          className={style.CompanyItemBox}
+          onClick={() => navigate(`/shop?brand=jordan`)}
+        >
           <div className={style.CompanyItem}>
             <img src={NikeLogo} alt="" />
           </div>
           <div className={style.CompanyItemTitle}>Jordan</div>
         </div>
-        <div className={style.CompanyItemBox} onClick={() => navigate(`/shop?brand=jordan`)}>
+        <div
+          className={style.CompanyItemBox}
+          onClick={() => navigate(`/shop?brand=jordan`)}
+        >
           <div className={style.CompanyItem}>
             <img src={AdidasLogo} alt="" />
           </div>
           <div className={style.CompanyItemTitle}>Jordan</div>
         </div>
-        <div className={style.CompanyItemBox} onClick={() => navigate(`/shop?brand=nike`)}>
+        <div
+          className={style.CompanyItemBox}
+          onClick={() => navigate(`/shop?brand=nike`)}
+        >
           <div className={style.CompanyItem}>
             <img src={NikeLogo} alt="" />
           </div>
           <div className={style.CompanyItemTitle}>Jordan</div>
         </div>
-        <div className={style.CompanyItemBox} onClick={() => navigate(`/shop?category=anime`)}>
+        <div
+          className={style.CompanyItemBox}
+          onClick={() => navigate(`/shop?category=anime`)}
+        >
           <img src={CustomNike} alt="" />
         </div>
       </div>
       <div className={style.NewArrivals}>
         <h1 className={style.NewArrivalsSliderTitle}>New Arrivals</h1>
         <div className={style.Slider}>
-        <div className={style.cards}>
+          <div className={style.cards}>
             {products.map((product: Object, index: number) => {
               return (
-                  <div key={index} className={style.container}>
-                     <img
-                        src={product.images[0]}
-                        className={style.shoes__image}
-                        onClick={() => navigate(`/product/${product._id}`)}
-                      />
-                      <div className={style.shoes__name}>{product.title}</div>
-                      <div className={style.shoes__price}>
-                        {product.price.originalPrice}
-                      </div>
-                  </div>
+                <div key={index} className={style.container}>
+                  <img
+                    src={product.images[0]}
+                    className={style.shoes__image}
+                    onClick={() => navigate(`/product/${product._id}`)}
+                  />
+                  <div className={style.shoes__name}>{product.title}</div>
+                  <div className={style.shoes__price}>{product.price}</div>
+                </div>
               );
             })}
           </div>
-          </div>
-
+        </div>
       </div>
       <div className={style.BestSellerSlider}>
         <h1 className={style.BestSellerSliderHeading}>Best Seller</h1>
         <div className={style.Slider}>
-        <div className={style.cards}>
+          <div className={style.cards}>
             {products.map((product: Object, index: number) => {
               return (
                 <div key={index}>
@@ -123,7 +152,7 @@ const Home = () => {
               );
             })}
           </div>
-          </div>
+        </div>
       </div>
       <HeroSection />
     </Container>
