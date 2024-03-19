f00117af98e50a58b63a7f466ec4f9dabc89cf13
ankitgupta-13 <guptankit0522@gmail.com>
2024-03-19 23:31:50 +0530

    Add missing import statement

--- Frontend/src/components/ImageSlider/ImageSlider.tsx
+++ Frontend/src/components/ImageSlider/ImageSlider.tsx
@@ -5,6 +5,7 @@ import ArrowBackIosNewIcon from "@mui/icons-material/ArrowBackIosNew";
 type ImageSliderProps = {
   imageUrls: string[];
 };
+
 const ImageSlider = ({ imageUrls }: ImageSliderProps) => {
   const [imageIndex, setImageIndex] = useState(0);
   console.log(imageIndex);
