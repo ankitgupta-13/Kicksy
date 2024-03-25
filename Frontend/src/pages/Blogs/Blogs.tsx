fe06c249761a61718a70f8dac7f7c590f5d40a7e
AyushAwasthi2384 <ayushawasthicoding@gmail.com>
2024-03-25 21:39:48 +0530

    ProdDesc resp done check

--- Frontend/src/pages/Blogs/Blogs.tsx
+++ Frontend/src/pages/Blogs/Blogs.tsx
@@ -17,32 +17,32 @@ const Blog = () => {
     getBlogs();
   }, []);
 
-  
+
   return (
-<div className={style.container}>
+    <div className={style.container}>
       <div className={style.topbar}>
-   <span className={style.topbar__heading}>Blog</span>
-   <span className={style.topbar__subheading}>Home - <span >All Posts</span></span>
+        <span className={style.topbar__heading}>Blog</span>
+        <span className={style.topbar__subheading}>Home - <span >All Posts</span></span>
       </div>
       <div className={style.bloglist}>
-         {bloglist.map((blog)=>(
-          <BlogCard blog={blog}/>
-         ))}
+        {bloglist.map((blog) => (
+          <BlogCard blog={blog} />
+        ))}
       </div>
+
       <div className={style.recent_blogs}>
         <div className={style.recent_blogs_title} style={{
           fontSize: "1rem",
-        
         }}>Stay-up-to-date</div>
-        <h2 style = {{
+        <h2 style={{
           marginTop: "0.7rem",
           fontSize: "2.5rem",
         }}>RECENT POSTS</h2>
-     <div className={style.recent__blogs_container}>
-       <PostCard/>
-      </div>
+        <div className={style.recent__blogs_container}>
+          <PostCard />
+        </div>
 
-    </div>
+      </div>
     </div>
   )
 }
