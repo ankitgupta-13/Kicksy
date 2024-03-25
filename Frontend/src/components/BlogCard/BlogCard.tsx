b35d667a61ea3042f5a85c91757aef07d3d9995a
Nishant Jaiswal <72241228+jaiswalnj@users.noreply.github.com>
2024-03-26 00:17:13 +0530

    blog card fixed

--- Frontend/src/components/BlogCard/BlogCard.tsx
+++ Frontend/src/components/BlogCard/BlogCard.tsx
@@ -1,17 +1,18 @@
 import { useNavigate } from "react-router-dom";
 import style from './BlogCard.module.css'
-import DummyImage from '../../assets/dummy_blog_image.png'
+
 const BlogCard = ({ blog }) => {
   const navigate = useNavigate();
+  const createdDate = blog?.createdAt?.split("T")[0];
   return (
     <div className={style.card} onClick={() => navigate(`/blogpage?blog=${blog._id}`)}>
       <img src={blog.imageurl} className={style.image} alt="Blog image" />
       <div className={style.card__info}>
-        <a>Games</a>
+        <a>{blog.category}</a>
         <span className={style.card__info_entity}>&#9679;</span>
-        <span className={style.card__info_date}>Feb 18, 2020</span>
+        <span className={style.card__info_date}>{createdDate}</span>
       </div>
-      <p className={style.card__paragraph}>Lorem ipsum dolor sit amet consectetur.</p>
+      <p className={style.card__paragraph}>{blog.blogTitle}</p>
     </div>
   )
 }
